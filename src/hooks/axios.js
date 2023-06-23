a6477193433cfb3f47d45e9a5a3a3e43e94c42f3
ahmedali-dev <mohamedali850100@gmail.com>
2023-06-23 16:20:10 +0100

    add more feture

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const url = "https://7yrwdl-5000.csb.app";
+const url = "https://sppy53-5000.csb.app/";
 const url2 = "http://localhost:5000";
 const instance = axios.create({
-  baseURL: url2,
+  baseURL: url,
   method: "POST",
 });
 
