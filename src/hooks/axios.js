7170bdd4d15d5f9a79222a79696fdfcc2c0cf798
ahmedali-dev <104825374+ahmedali-dev@users.noreply.github.com>
2023-08-15 11:39:55 +0300

    Update axios.js

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url2 = "https://node-to-do-alpha.vercel.app/";
+const url2 = "https://node-to-do-n1zx.vercel.app/";
 const instance = axios.create({
   baseURL: url2,
   method: "POST",
